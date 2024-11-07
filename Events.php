176f82d2d43eb83dfe74b2e552a77a0e7bc4298e
Harald Bamberger <harald.bamberger@technikum-wien.at>
2024-11-07 19:20:09 +0100

    add dependecy on legacy code

--- Events.php
+++ Events.php
@@ -9,7 +9,12 @@ Events::on('lvMenuBuild', function ($menu_reference,$params) {
 
 	// extracts all key=>value pairs of the associative array as variables in the current scope 
 	extract($params);
-
+	require_once(FHCPATH.'include/benutzerberechtigung.class.php');
+	if(!isset($rechte))
+	{
+		$rechte = new benutzerberechtigung();
+		$rechte->getBerechtigungen($user);
+	}
 	$menu =& $menu_reference();
 	$addon_lvinfo_col = array();
 	require_once(dirname(__FILE__).'/cis/menu_lv.inc.php');
